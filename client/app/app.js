022bd576926fedb856d972f3194aa0f98b4bb087
Mark Pieszak <mpieszak84@gmail.com>
2015-10-28 10:53:03 -0400

    Added files

--- client/app/app.js
+++ client/app/app.js
@@ -3,12 +3,12 @@
 	var AppComponent = ng
 		.Component({
 			selector: 'my-app',
-			template: '<h1>My First Angular 2 App</h1>'
+			template: '<h1>Basic Component</h1>'
 		})
 		.Class({
 			constructor: function() {}
 		});
-        
+
 	document.addEventListener('DOMContentLoaded', function() {
 		ng.bootstrap(AppComponent);
 	});
