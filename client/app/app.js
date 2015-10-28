1c0a86da46df7706d071dd8f82537e1250647853
Mark Pieszak <mpieszak84@gmail.com>
2015-10-28 12:37:42 -0400

    Added todo list

--- client/app/app.js
+++ client/app/app.js
@@ -1,13 +1,41 @@
 (function() {
 
+console.log(ng)
 	var AppComponent = ng
 		.Component({
-			selector: 'my-app',
-			template: '<h1>Basic Component</h1>'
+			selector: 'todo-list',
+			template: '<ul>' +
+				'<li *ng-for="#todo of todos">' +
+				'{{ todo }}' +
+                '<button (click)="removeToDo($index)">X</button>' +
+				'</li>' +
+				'</ul>' +
+				'<input #textbox (keyup)="doneTyping($event)">' +
+				'<button (click)="addTodo(textbox.value)">Add Todo</button>',
+            directives: [ng.NgFor]
 		})
 		.Class({
-			constructor: function() {}
+			constructor: function() {
+                this.todos = ["Eat Breakfast", "Walk Dog", "Breathe"];
+
+        		this.addTodo = function(todo) {
+        			this.todos.push(todo);
+        		};
+
+                this.removeToDo = function (index) {
+                    this.todos.splice(index, 1);
+                };
+
+        		this.doneTyping = function($event) {
+        			if($event.which === 13) {
+        				this.addTodo($event.target.value);
+        				$event.target.value = null;
+        			}
+        		};
+
+			}
 		});
+
 
 	document.addEventListener('DOMContentLoaded', function() {
 		ng.bootstrap(AppComponent);
