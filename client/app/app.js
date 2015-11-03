9c4895bea7a2772b804c04c9acc308741c01e137
Mark Pieszak <mpieszak84@gmail.com>
2015-11-03 14:36:47 -0500

    Tweaks

--- client/app/app.js
+++ client/app/app.js
@@ -1,51 +1,24 @@
-'use strict';
-
-;(function() {
-
-console.log(ng)
-	var AppComponent = ng
-		.Component({
-			selector: 'todo-list',
-			template: '<ul>' +
-				'<li *ng-for="#todo of todos">' +
-				'{{ todo }}' +
-                '<button (click)="removeToDo($index)">X</button>' +
-				'</li>' +
-				'</ul>' +
-				'<input #textbox (keyup)="doneTyping($event)">' +
-				'<button (click)="addTodo(textbox.value)">Add Todo</button>' +
-                '<br><br><br>' +
-                '<div>{{ todos | json }}</div>',
-            directives: [ng.NgFor]
-		})
-		.Class({
-			constructor: function() {
-                this.todos = ["Eat Breakfast", "Walk Dog", "Breathe"];
-
-        		this.addTodo = function(todo) {
-        			this.todos.push(todo);
-        		};
-
-                this.removeToDo = function (index) {
-                    console.log(index);
-                    this.todos.splice(index, 1);
-                };
-
-        		this.doneTyping = function($event) {
-        			if($event.which === 13) {
-        				this.addTodo($event.target.value);
-        				$event.target.value = null;
-        			}
-        		};
-
-			}
-		});
-
-
-	// Bootstrap Angular 2
-	// To do: Create the masterView here
-	document.addEventListener('DOMContentLoaded', function() {
-		ng.bootstrap(AppComponent);
-	});
-
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var angular2_1 = require('angular2/angular2');
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        angular2_1.Component({
+            selector: 'my-app',
+            template: '<h1>My First Angular 2 App</h1><br>'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
 })();
+angular2_1.bootstrap(AppComponent);
+//# sourceMappingURL=app.js.map
